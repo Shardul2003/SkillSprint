b0d0b5c1218dc7c4ba60c9672dbb98a9141a71f4
Shardul Dhongade <shardul21@vt.edu>
2024-09-15 10:09:14 -0400

    Cleanup files + prep

--- backend/job-link-scraper/youtube_api.py
+++ backend/job-link-scraper/youtube_api.py
@@ -4,11 +4,26 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# def get_youtube_resources():
+#     search_url = "https://www.googleapis.com/youtube/v3/search"
+#     params = {
+#         "part": "snippet",
+#         "q": f"react.js tutorial",
+#         "key": os.getenv("YOUTUBE_API_KEY"),
+#         "type": "video",
+#         "maxResults": 2
+#     }
+#     response = requests.get(search_url, params=params)
+#     print(response)
+#     videos = response.json().get('items', [])
+#     resources = [{"name": video['snippet']['title'], "url": f"https://www.youtube.com/watch?v={video['id']['videoId']}"} for video in videos]
+#     return resources
+
 def get_youtube_resources(tech):
     search_url = "https://www.googleapis.com/youtube/v3/search"
     params = {
         "part": "snippet",
-        "q": f"react.js tutorial",
+        "q": f"{tech} tutorial",
         "key": os.getenv("YOUTUBE_API_KEY"),
         "type": "video",
         "maxResults": 2
@@ -17,5 +32,7 @@ def get_youtube_resources(tech):
     videos = response.json().get('items', [])
     resources = [{"name": video['snippet']['title'], "url": f"https://www.youtube.com/watch?v={video['id']['videoId']}"} for video in videos]
     return resources
+
+# print(os.getenv("YOUTUBE_API_KEY"))
 
 print(get_youtube_resources("react"))
